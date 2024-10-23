dfc09d195b61412850721a5edf91651abedf3395
Revaz Mustoyan <revaz.mustoyan@yandex.ru>
2024-10-23 12:35:33 +0300

    v1.1

--- yandex_praktikum/burger.py
+++ yandex_praktikum/burger.py
@@ -1,7 +1,7 @@
 from typing import List
 
-from praktikum.bun import Bun
-from praktikum.ingredient import Ingredient
+from yandex_praktikum.praktikum import Bun
+from yandex_praktikum.praktikum import Ingredient
 
 
 class Burger:
